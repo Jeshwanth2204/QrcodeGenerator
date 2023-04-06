9978f96b1bbbd5ea72bbdfdcf1fe763d93616b01
Jeshwanth. S <124120554+Jeshwanth2204@users.noreply.github.com>
2023-04-06 14:36:58 +0530

    Update Qrcode generator.py

--- Qrcode generator.py
+++ Qrcode generator.py
@@ -23,16 +23,20 @@ Image_view.pack(padx = 50,pady = 10,side = RIGHT)
 title_label = Label(root,text = 'Title',fg = 'white',bg = '#ae2321',font = ('bold',17))
 title_label.place(x = 50 , y = 170)
 
+title2_label = Label(root,text = "Link",fg = 'white',bg = '#ae2321',font = ('bold',17))
+title2_label.place(x = 48, y = 250) 
+                   
 entry_input_name = Entry(root,width = 13,font=('arial',15))
 entry_input_name.place(x = 50, y = 200)
 
 entry_input_link = Entry(root,width = 28,font =('arial',15))
-entry_input_link.place(x = 50,y = 250)
+entry_input_link.place(x = 50,y = 280)
 
 generate_button = Button(root,text = 'Generate',width = 20,height = 2,bg = 'black',fg = 'white',command = generate)
-generate_button.place(x = 50 ,y = 300)
+generate_button.place(x = 50 ,y = 350)
 
   
 root.mainloop()
+
 
 
